74e11d5287ce2dbb25ab9139cb9cd24240817454
Frederico Filho <fredhave17@gmail.com>
2023-06-16 19:13:18 -0300

    chore: added text tests

--- App.tsx
+++ App.tsx
@@ -8,6 +8,12 @@ function App(): JSX.Element {
     <SafeAreaView>
       <Text variant="headingLarge">Test Text</Text>
       <Text variant="headingMedium">Test Text</Text>
+      <Text variant="headingSmall">Test Text</Text>
+      <Text variant="paragraphLarge">Test Text</Text>
+      <Text variant="paragraphMedium">Test Text</Text>
+      <Text variant="paragraphSmall">Test Text</Text>
+      <Text variant="paragraphCaption">Test Text</Text>
+      <Text variant="paragraphCaptionSmall">Test Text</Text>
     </SafeAreaView>
   );
 }
