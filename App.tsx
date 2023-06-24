5fae35b11748235828894b34a83226fc81f736a4
Frederico Filho <fredhave17@gmail.com>
2023-06-23 22:08:08 -0300

    feat: created a home screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
@@ -10,9 +13,35 @@ function App(): JSX.Element {
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text variant="headingLarge">Test Text</Text>
+          <Text variant="headingLarge" marginBottom="s8">
+            Ola
+          </Text>
+          <Text variant="paragraphLarge" marginBottom="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
 
-          <Icon color="backgroundContrast" name={'heartFill'} />
+          <TextInput
+            label={'E-mail'}
+            placeholder="Digite seu e-mail"
+            errorMessage="Error mensage"
+            boxProps={{marginBottom: 's16'}}
+          />
+
+          <TextInput
+            label={'Senha'}
+            placeholder="Digite sua senha"
+            RightComponent={<Icon name="eyeOn" color="gray2" />}
+          />
+
+          <Text
+            variant="paragraphSmall"
+            bold
+            color="primary"
+            marginBottom="s40">
+            Esqueci minha senha
+          </Text>
+          <Button title="Entrar" style={{marginBottom: 12}} />
+          <Button variant="outline" title="Criar uma conta" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
