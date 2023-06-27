147861a36c669b5967b56906f9550e2ca61663a1
Frederico Filho <fredhave17@gmail.com>
2023-06-26 23:08:51 -0300

    feat: added screen on app

--- App.tsx
+++ App.tsx
@@ -1,50 +1,20 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Text} from './src/components/Text/Text';
+
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Box} from './src/components/Box/Box';
-import {Button} from './src/components/Button/Button';
-import {TextInput} from './src/components/TextInput/TextInput';
-import {Icon} from './src/components/Icon/Icon';
+
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text variant="headingLarge" marginBottom="s8">
-            Ola
-          </Text>
-          <Text variant="paragraphLarge" marginBottom="s40">
-            Digite seu e-mail e senha para entrar
-          </Text>
-
-          <TextInput
-            label={'E-mail'}
-            placeholder="Digite seu e-mail"
-            errorMessage="Error mensage"
-            boxProps={{marginBottom: 's16'}}
-          />
-
-          <TextInput
-            label={'Senha'}
-            placeholder="Digite sua senha"
-            RightComponent={<Icon name="eyeOn" color="gray2" />}
-          />
-
-          <Text
-            variant="paragraphSmall"
-            bold
-            color="primary"
-            marginBottom="s40">
-            Esqueci minha senha
-          </Text>
-          <Button title="Entrar" style={{marginBottom: 12}} />
-          <Button variant="outline" title="Criar uma conta" />
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        {/* <LoginScreen /> */}
+        <SignUpScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
