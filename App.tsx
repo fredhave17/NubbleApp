c2b90ee53b0e647b423d729bcf8d1efed2360a12
Frederico Filho <fredhave17@gmail.com>
2024-02-26 22:54:01 -0300

    feat: created button component

--- App.tsx
+++ App.tsx
@@ -9,14 +9,23 @@ import React from 'react';
 
 import {SafeAreaView} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
+import {Button} from './src/components/Button/Button';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingMedium" italic bold>
-        Coffstack
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingMedium" italic bold>
+          Coffstack
+        </Text>
+        <Button title="Entrar" margin="s12" preset="primary" />
+        <Button title="Entrar" margin="s12" preset="outline" />
+        <Button title="Entrar" margin="s12" loading />
+        <Button title="Entrar" margin="s12" disabled />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
